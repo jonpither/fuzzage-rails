2a44e54daf98601e647b10f82e1f8549a6fe6919
jon <jon@3fa4c1bb-60a5-443b-ae1f-8747380605b4>
2008-11-18 20:20:53 +0000

    added some ignores

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,16 @@ class MainController < ApplicationController
     end
 
     def register
-        @user = User.new
+        if request.get?
+            @user = User.new
+        else
+            @user = User.new(params[:user])
+
+            if @user.save
+                flash[:notice] = "Thank you for registering! We have sent a confirmation email to #{@user.email} with instructions on how to validate your account."
+                redirect_to(:action => "index")
+            end
+        end
     end
 
     def confirm_email
--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
-class MainControllerControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+class MainControllerTest < ActionController::TestCase
+    def test_new_user_created_when_get_request_is_made
+        get :register
+        user = assigns(:user)
+
+        assert user.new_record?
+        assert_nil user.id
+        assert_nil user.name
+        assert_nil user.email
+
+        assert_response :success
+    end
+
+    def test_new_user_saved_when_post_request_is_made
+        post :register, :user => { :name=>'bastard', :email=>'foo@foo.com', :password=>'password', :confirm_password=>'password'}
+
+        user = assigns(:user)
+
+        assert !user.new_record?
+        assert_redirected_to :action => "index"
+    end
 end
