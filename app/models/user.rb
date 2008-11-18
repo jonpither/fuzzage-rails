19b3859eac88d1ad389a4af2ee19af56dac0bb8a
jon <jon@3fa4c1bb-60a5-443b-ae1f-8747380605b4>
2008-11-18 19:31:12 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require "digest/sha1"
+
 class User < ActiveRecord::Base
     attr_accessor :password, :confirm_password
 
@@ -8,7 +10,26 @@ class User < ActiveRecord::Base
     def validate_on_create
         @email_format = Regexp.new(/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/)
         errors.add(:email, "must be a valid format") unless email == nil || @email_format.match(email)
-#        errors.add(:confirm_password, "does not match") unless password == confirm_password
-#        errors.add(:password, "cannot be blank") unless !password or password.length > 0
+        errors.add(:confirm_password, "does not match") unless password == confirm_password
+        errors.add(:password, "can't be blank") if !password or password.length < 1
     end
+
+    # hash password before create
+    def before_create
+        self.hashed_password = User.hash_password(self.password)
+    end
+
+    # after creation, clear password from memory
+    def after_create
+      @password = nil
+      @confirm_password = nil
+    end
+
+    private
+
+    # hash password for storage in database
+    def self.hash_password(password)
+        Digest::SHA1.hexdigest(password)
+    end
+
 end
