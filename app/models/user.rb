5dba6b6d37aa8fc0ddda4cb959b73a485f1c74b4
jon <jon@3fa4c1bb-60a5-443b-ae1f-8747380605b4>
2008-11-18 20:50:08 +0000

    registration stuff is in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
     attr_accessor :password, :confirm_password
 
     validates_presence_of :name, :email
-    validates_length_of :name, :maximum => 100, :allow_nil => true
-    validates_length_of :name, :minimum => 2,  :allow_nil => true
+    validates_length_of :name, :maximum => 100,  :allow_blank => true
+    validates_length_of :name, :minimum => 2,  :allow_blank => true
 
     def validate_on_create
         @email_format = Regexp.new(/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/)
-        errors.add(:email, "must be a valid format") unless email == nil || @email_format.match(email)
+        errors.add(:email, "must be a valid format") unless email == nil || email.length < 1 || @email_format.match(email)
         errors.add(:confirm_password, "does not match") unless password == confirm_password
         errors.add(:password, "can't be blank") if !password or password.length < 1
     end
